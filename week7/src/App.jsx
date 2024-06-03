827d07fcc31c6de7a94e8bb88982d44c2d70b0c6
ajay8182831490 <77184625+ajay8182831490@users.noreply.github.com>
2024-06-03 16:53:49 +0530

    router done

--- week7/src/App.jsx
+++ week7/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Routes, Route, BrowserRouter, useNavigate } from 'react-router-dom'
+
 import './App.css'
 
 function App() {
@@ -8,28 +9,51 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <h1> hi there</h1>
+      <BrowserRouter>
+        <Appheader></Appheader>
+        <Routes>
+          <Route path='/' element={<Loading />}>
+
+          </Route>
+          <Route path='/dash' element={<Dashboard />}>
+
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
+
+function Appheader() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <button onClick={() => {
+        navigate('/');
+      }}> Loading page</button>
+      <button onClick={() => {
+        navigate('/dash');
+      }}> Dashboard page</button>
+    </>
+  )
+}
+function Loading() {
+  return (
+    <div>
+      <h1>this is a loading page</h1>
+    </div>
+  )
+}
+function Dashboard() {
+  return (
+    <div>
+      <h1>this is an dashboard page</h1>
+    </div>)
+}
+
 
 export default App
