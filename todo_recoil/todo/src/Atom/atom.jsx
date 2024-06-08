261c079dd105e03f6a3e5a207f829ff18a7b1fa2
ajay8182831490 <77184625+ajay8182831490@users.noreply.github.com>
2024-06-08 10:44:06 +0530

    todo application complteted by using recoil state managemnt library

---
+++ todo_recoil/todo/src/Atom/atom.jsx
@@ -0,0 +1,50 @@
+// todo.jsx
+
+import { atom, selector } from 'recoil';
+
+const todoListState = atom({
+    key: 'TodoList',
+    default: [],
+});
+
+const todoListFilterState = atom({
+    key: 'TodoListFilter',
+    default: 'Show All',
+});
+
+const filteredTodoListState = selector({
+    key: 'FilteredTodoList',
+    get: ({ get }) => {
+        const filter = get(todoListFilterState);
+        const list = get(todoListState);
+
+        switch (filter) {
+            case 'Show Completed':
+                return list.filter((item) => item.isComplete);
+            case 'Show Uncompleted':
+                return list.filter((item) => !item.isComplete);
+            default:
+                return list;
+        }
+    },
+});
+
+const todoListStatsState = selector({
+    key: 'TodoListStats',
+    get: ({ get }) => {
+        const todoList = get(todoListState);
+        const totalNum = todoList.length;
+        const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
+        const totalUncompletedNum = totalNum - totalCompletedNum;
+        const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum * 100;
+
+        return {
+            totalNum,
+            totalCompletedNum,
+            totalUncompletedNum,
+            percentCompleted,
+        };
+    },
+});
+
+export { todoListState, todoListFilterState, filteredTodoListState, todoListStatsState };
